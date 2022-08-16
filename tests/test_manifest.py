0c035a5a65242cecb0e7ac95934bdc0d0bdbc210
Mary Lau <marylau@fb.com>
2022-08-16 10:04:04 -0700

    Add nonreplicated chunked tensor entry case to manifest unit test.
    Summary: Due to recent diffs D38295461 (https://github.com/pytorch/torchsnapshot/commit/ff34f0706b91f7b9e19531bfe5d9b6b31bf46d23) and D38646776 (https://github.com/pytorch/torchsnapshot/commit/7faf26c483bbb663fd9ea0a5c0f7f999ff3e97c1), torchsnapshot now automatically does chunking for both replicated and nonreplicated tensors. I added nonreplicated ChunkedTensorEntry to test_manifest.py and updated the unit tests to check that nonreplicated chunked objects are correctly read from the manifest. Moreover, I added a case checking rank = 1, to further confirm that nonreplicated chunked entry is being differently read between ranks 0 and 1.
    
    Reviewed By: tangbinh
    
    Differential Revision: D38714492
    
    fbshipit-source-id: 59dbeacf691c4484f51dcf6d5f3700f88d496e40

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -19,7 +19,9 @@ from torchsnapshot.manifest import (
 )
 
 _MANIFEST = {
-    "0/foo": DictEntry(keys=["bar", "baz", "qux"]),
+    "0/foo": DictEntry(
+        keys=["bar", "baz", "qux", "quuux", "qux_chunked", "quux_chunked"]
+    ),
     "0/foo/bar": ObjectEntry(
         location="0/foo/bar", serializer="torch_save", obj_type="Bar", replicated=False
     ),
@@ -46,38 +48,6 @@ _MANIFEST = {
     ),
     "0/foo/quux": TensorEntry(
         location="0/foo/quux",
-        serializer="torch_save",
-        dtype="float32",
-        shape=[128, 128],
-        replicated=False,
-    ),
-    "1/foo": DictEntry(keys=["bar", "baz", "qux"]),
-    "1/foo/bar": ObjectEntry(
-        location="1/foo/bar", serializer="torch_save", obj_type="Bar", replicated=False
-    ),
-    "1/foo/baz": ObjectEntry(
-        location="replicated/foo/baz",
-        serializer="torch_save",
-        obj_type="Baz",
-        replicated=True,
-    ),
-    "1/foo/qux": ShardedTensorEntry(
-        shards=[
-            Shard(
-                offsets=[4, 0],
-                sizes=[4, 4],
-                tensor=TensorEntry(
-                    location="sharded/foo/qux.1",
-                    serializer="torch_save",
-                    dtype="float32",
-                    shape=[2, 8],
-                    replicated=False,
-                ),
-            )
-        ]
-    ),
-    "1/foo/quux": TensorEntry(
-        location="1/foo/quux",
         serializer="torch_save",
         dtype="float32",
         shape=[128, 128],
@@ -112,6 +82,69 @@ _MANIFEST = {
         ],
         replicated=True,
     ),
+    "0/foo/quux_chunked": ChunkedTensorEntry(
+        dtype="float32",
+        shape=[100],
+        chunks=[
+            Shard(
+                offsets=[0],
+                sizes=[50],
+                tensor=TensorEntry(
+                    location="0/foo/qux_chunked_0",
+                    serializer="torch_save",
+                    dtype="float32",
+                    shape=[50],
+                    replicated=False,
+                ),
+            ),
+            Shard(
+                offsets=[50],
+                sizes=[50],
+                tensor=TensorEntry(
+                    location="0/foo/qux_chunked_50",
+                    serializer="torch_save",
+                    dtype="float32",
+                    shape=[50],
+                    replicated=False,
+                ),
+            ),
+        ],
+        replicated=False,
+    ),
+    "1/foo": DictEntry(
+        keys=["bar", "baz", "qux", "quuux", "qux_chunked", "quux_chunked"]
+    ),
+    "1/foo/bar": ObjectEntry(
+        location="1/foo/bar", serializer="torch_save", obj_type="Bar", replicated=False
+    ),
+    "1/foo/baz": ObjectEntry(
+        location="replicated/foo/baz",
+        serializer="torch_save",
+        obj_type="Baz",
+        replicated=True,
+    ),
+    "1/foo/qux": ShardedTensorEntry(
+        shards=[
+            Shard(
+                offsets=[4, 0],
+                sizes=[4, 4],
+                tensor=TensorEntry(
+                    location="sharded/foo/qux.1",
+                    serializer="torch_save",
+                    dtype="float32",
+                    shape=[2, 8],
+                    replicated=False,
+                ),
+            )
+        ]
+    ),
+    "1/foo/quux": TensorEntry(
+        location="1/foo/quux",
+        serializer="torch_save",
+        dtype="float32",
+        shape=[128, 128],
+        replicated=False,
+    ),
     "1/foo/qux_chunked": ChunkedTensorEntry(
         dtype="float32",
         shape=[7, 10],
@@ -124,7 +157,7 @@ _MANIFEST = {
                     serializer="torch_save",
                     dtype="float32",
                     shape=[5, 10],
-                    replicated=True,
+                    replicated=False,
                 ),
             ),
             Shard(
@@ -135,11 +168,40 @@ _MANIFEST = {
                     serializer="torch_save",
                     dtype="float32",
                     shape=[2, 10],
-                    replicated=True,
+                    replicated=False,
                 ),
             ),
         ],
         replicated=True,
+    ),
+    "1/foo/quux_chunked": ChunkedTensorEntry(
+        dtype="float32",
+        shape=[100],
+        chunks=[
+            Shard(
+                offsets=[0],
+                sizes=[50],
+                tensor=TensorEntry(
+                    location="1/foo/qux_chunked_0",
+                    serializer="torch_save",
+                    dtype="float32",
+                    shape=[50],
+                    replicated=False,
+                ),
+            ),
+            Shard(
+                offsets=[50],
+                sizes=[50],
+                tensor=TensorEntry(
+                    location="1/foo/qux_chunked_50",
+                    serializer="torch_save",
+                    dtype="float32",
+                    shape=[50],
+                    replicated=False,
+                ),
+            ),
+        ],
+        replicated=False,
     ),
 }
 
@@ -158,7 +220,7 @@ class ManifestTest(unittest.TestCase):
         loaded_metadata = SnapshotMetadata.from_yaml(yaml_str=yaml_str)
         self.assertDictEqual(metadata.manifest, loaded_metadata.manifest)
 
-    def test_load_with_same_world_size(self) -> None:
+    def test_load_with_same_world_size_rank_zero(self) -> None:
         available_entries = get_available_entries(_MANIFEST, 0)
         expected_available_entries = {
             "foo/bar": ObjectEntry(
@@ -234,6 +296,144 @@ class ManifestTest(unittest.TestCase):
                     ),
                 ],
                 replicated=True,
+            ),
+            "foo/quux_chunked": ChunkedTensorEntry(
+                dtype="float32",
+                shape=[100],
+                chunks=[
+                    Shard(
+                        offsets=[0],
+                        sizes=[50],
+                        tensor=TensorEntry(
+                            location="0/foo/qux_chunked_0",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[50],
+                            replicated=False,
+                        ),
+                    ),
+                    Shard(
+                        offsets=[50],
+                        sizes=[50],
+                        tensor=TensorEntry(
+                            location="0/foo/qux_chunked_50",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[50],
+                            replicated=False,
+                        ),
+                    ),
+                ],
+                replicated=False,
+            ),
+        }
+        self.assertDictEqual(available_entries, expected_available_entries)
+
+    def test_load_with_same_world_size_rank_one(self) -> None:
+        available_entries = get_available_entries(_MANIFEST, 1)
+        expected_available_entries = {
+            "foo/bar": ObjectEntry(
+                location="1/foo/bar",
+                serializer="torch_save",
+                obj_type="Bar",
+                replicated=False,
+            ),
+            "foo/baz": ObjectEntry(
+                location="replicated/foo/baz",
+                serializer="torch_save",
+                obj_type="Baz",
+                replicated=True,
+            ),
+            "foo/qux": ShardedTensorEntry(
+                shards=[
+                    Shard(
+                        offsets=[0, 0],
+                        sizes=[4, 4],
+                        tensor=TensorEntry(
+                            location="sharded/foo/qux.0",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[2, 8],
+                            replicated=False,
+                        ),
+                    ),
+                    Shard(
+                        offsets=[4, 0],
+                        sizes=[4, 4],
+                        tensor=TensorEntry(
+                            location="sharded/foo/qux.1",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[2, 8],
+                            replicated=False,
+                        ),
+                    ),
+                ]
+            ),
+            "foo/quux": TensorEntry(
+                location="1/foo/quux",
+                serializer="torch_save",
+                dtype="float32",
+                shape=[128, 128],
+                replicated=False,
+            ),
+            "foo/qux_chunked": ChunkedTensorEntry(
+                dtype="float32",
+                shape=[7, 10],
+                chunks=[
+                    Shard(
+                        offsets=[0, 0],
+                        sizes=[5, 10],
+                        tensor=TensorEntry(
+                            location="replicated/foo/qux_chunked_0_0",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[5, 10],
+                            replicated=False,
+                        ),
+                    ),
+                    Shard(
+                        offsets=[5, 0],
+                        sizes=[2, 10],
+                        tensor=TensorEntry(
+                            location="replicated/foo/qux_chunked_5_0",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[2, 10],
+                            replicated=False,
+                        ),
+                    ),
+                ],
+                replicated=True,
+            ),
+            "foo/quux_chunked": ChunkedTensorEntry(
+                dtype="float32",
+                shape=[100],
+                chunks=[
+                    Shard(
+                        offsets=[0],
+                        sizes=[50],
+                        tensor=TensorEntry(
+                            location="1/foo/qux_chunked_0",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[50],
+                            replicated=False,
+                        ),
+                    ),
+                    Shard(
+                        offsets=[50],
+                        sizes=[50],
+                        tensor=TensorEntry(
+                            location="1/foo/qux_chunked_50",
+                            serializer="torch_save",
+                            dtype="float32",
+                            shape=[50],
+                            replicated=False,
+                        ),
+                    ),
+                ],
+                replicated=False,
             ),
         }
         self.assertDictEqual(available_entries, expected_available_entries)
