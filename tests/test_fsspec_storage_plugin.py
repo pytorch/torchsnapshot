5edff085663f17b8197756214500cc937a4d59f0
chengcshi <chengcshi@tencent.com>
2022-10-21 16:43:22 +0800

    tiny polish

--- tests/test_fsspec_storage_plugin.py
+++ tests/test_fsspec_storage_plugin.py
@@ -6,39 +6,38 @@
 # LICENSE file in the root directory of this source tree.
 import io
 import logging
-import unittest
 import uuid
 
+import pytest
 import torch
 
-from torchsnapshot.io_types import ReadIO, WriteIO
+from torchsnapshot.io_types import WriteIO, ReadIO
 from torchsnapshot.storage_plugins.fsspec import FSSpecPlugin
-from torchsnapshot.test_utils import async_test
 
 logger: logging.Logger = logging.getLogger(__name__)
 
-_TEST_BUCKET = "torchsnapshot-test"
-_TENSOR_SZ = int(100_000_000 / 4)
+_TEST_BUCKET = "chengcshi"
+# _TENSOR_SZ = int(100_000_000 / 4)
+_TENSOR_SZ = 10
 
 
-class FSSpecStoragePluginTest(unittest.TestCase):
-    @async_test
-    async def test_write_read_delete(self) -> None:
-        path = f"{_TEST_BUCKET}/{uuid.uuid4()}"
-        logger.info(path)
-        plugin = FSSpecPlugin(root=path, protocol="s3")
+@pytest.mark.asyncio
+async def test_fsspec_s3_write_read_delete() -> None:
+    path = f"{_TEST_BUCKET}/{uuid.uuid4()}"
+    logger.info(path)
+    plugin = FSSpecPlugin(root=path, protocol="s3")
 
-        tensor = torch.rand((_TENSOR_SZ,))
-        buf = io.BytesIO()
-        torch.save(tensor, buf)
-        write_io = WriteIO(path="tensor", buf=memoryview(buf.getvalue()))
+    tensor = torch.rand((_TENSOR_SZ,))
+    buf = io.BytesIO()
+    torch.save(tensor, buf)
+    write_io = WriteIO(path="tensor", buf=memoryview(buf.getvalue()))
 
-        await plugin.write(write_io=write_io)
+    await plugin.write(write_io=write_io)
 
-        read_io = ReadIO(path="tensor")
-        await plugin.read(read_io=read_io)
-        loaded = torch.load(read_io.buf)
-        assert torch.allclose(tensor, loaded)
+    read_io = ReadIO(path="tensor")
+    await plugin.read(read_io=read_io)
+    loaded = torch.load(read_io.buf)
+    assert torch.allclose(tensor, loaded)
 
-        await plugin.delete(path="tensor")
-        await plugin.close()
+    await plugin.delete(path="tensor")
+    await plugin.close()
--- torchsnapshot/storage_plugins/fsspec.py
+++ torchsnapshot/storage_plugins/fsspec.py
@@ -15,37 +15,20 @@ from torchsnapshot.io_types import StoragePlugin, ReadIO, WriteIO
 class FSSpecPlugin(StoragePlugin):
     def __init__(self, root: str, protocol: str, **storage_options) -> None:
         self.root = root
-        if protocol not in ["http", "s3"]:
-            raise ValueError(f"Protocol {protocol} does not support async")
         self.fs = fsspec.filesystem(protocol, asynchronous=True, **storage_options)
 
     async def write(self, write_io: WriteIO) -> None:
         path = os.path.join(self.root, write_io.path)
-        session = await self.fs.set_session()
-        with self.fs.open(path, 'wb') as f:
-            await f.write(write_io.buf)
-        await session.close()
+        await self.fs._pipe_file(path, bytes(write_io.buf))
 
     async def read(self, read_io: ReadIO) -> None:
         path = os.path.join(self.root, read_io.path)
-        byte_range = read_io.byte_range
-
-        session = await self.fs.set_session()
-        with self.fs.open(path, 'rb') as f:
-            if byte_range is None:
-                read_io.buf = io.BytesIO(await f.read())
-            else:
-                offset = byte_range[0]
-                size = byte_range[1] - byte_range[0]
-                await f.seek(offset)
-                read_io.buf = io.BytesIO(await f.read(size))
-        await session.close()
+        result = await self.fs._cat_file(path)
+        read_io.buf = io.BytesIO(result)
 
     async def delete(self, path: str) -> None:
         path = os.path.join(self.root, path)
-        session = await self.fs.set_session()
-        await self.fs.delete(path)
-        await session.close()
+        await self.fs._rm_file(path)
 
     async def close(self) -> None:
         pass
