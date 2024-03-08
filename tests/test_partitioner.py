514e43d80a8f2e2b2f854ce3577bf8a08b726dfa
Conner Nilsen <connernilsen@meta.com>
2024-03-08 14:19:27 -0800

    Pyre Configurationless migration for] [batch:231/244]
    Reviewed By: inseokhwang
    
    Differential Revision: D54686975
    
    fbshipit-source-id: 58759f1c69c8abc06cf32b9a9f406dc4c51de2d5

--- tests/test_partitioner.py
+++ tests/test_partitioner.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 from collections import defaultdict
 from pathlib import Path
 from typing import List, Tuple
